47f5e03251f0db49879c2ef0f23a330ee093648b
Jon Gunderson <jongund@illinois.edu>
2020-10-02 17:33:07 -0500

    udpated styles and template

--- js/banner-menu.js
+++ js/banner-menu.js
@@ -16,15 +16,32 @@ var DisclosureMenu = function(domNode) {
   var containerNodes = this.rootNode.querySelectorAll('ul.menu > li');
 
   this.menuContainers = [];
+  
+  this.firstMenuContainer = false;
+  this.lastMenuContainer = false;
 
-  for (var i = 0; i < containerNodes.length; i++) {
-    var containerNode = containerNodes[i];
+  var i = 0;
+
+  for (var j = 0; j < containerNodes.length; j++) {
+    var containerNode = containerNodes[j];
     var buttonNode = containerNode.querySelector('a');
+    if (!buttonNode) {
+        continue;
+    }
 
     this.menuContainers[i] = {};
     this.menuContainers[i].hasSubMenu = false;
     this.menuContainers[i].containerNode = containerNode;
     this.menuContainers[i].buttonNode = buttonNode;
+
+    if (!this.firstMenuContainer) {
+        this.firstMenuContainer = this.menuContainers[0];
+    }
+    this.lastMenuContainer = this.menuContainers[i];
+    
+    console.log('[buttonNode]: ' + buttonNode.textContent);
+    buttonNode.addEventListener('click', this.handleButtonClick.bind(this));
+    buttonNode.addEventListener('keydown', this.handleButtonKeydown.bind(this));
 
     // Updated properties and add event handlers
     containerNode.addEventListener('focusin', this.handleFocusIn.bind(this));
@@ -33,7 +50,9 @@ var DisclosureMenu = function(domNode) {
       this.menuContainers[i].hasSubMenu = true;
 
       var menuNode = containerNode.querySelector('.sub-menu');
-      var menuitemNodes = containerNode.querySelector('.sub-menu a');
+      var linkNodes = containerNode.querySelectorAll('.sub-menu a');
+      
+      console.log('[linkNodes]: ' + linkNodes.length);
 
       // Updated properties and add event handlers
       containerNode.addEventListener('focusin', this.handleFocusIn.bind(this));
@@ -45,18 +64,21 @@ var DisclosureMenu = function(domNode) {
       buttonNode.setAttribute('aria-controls', 'banner-menu-' + i);
 
       buttonNode.addEventListener('click', this.handleButtonClick.bind(this));
+      buttonNode.addEventListener('keydown', this.handleButtonKeydown.bind(this));
+      
+      for (let i = 0; i < linkNodes.length; i++) {
+          linkNodes[i].addEventListener('keydown', this.handleLinkKeydown.bind(this));
+      }
 
       menuNode.id = 'banner-menu-' + i;
 
       this.menuContainers[i].menuNode = menuNode;
-      this.menuContainers[i].menuitemNodes = menuitemNodes;
-      this.menuContainers[i].firstMenuitemNode = menuitemNodes[0];
-      this.menuContainers[i].lastMenuitemNode = menuitemNodes[menuitemNodes.length - 1];
-
+      this.menuContainers[i].linkNodes = linkNodes;
+      this.menuContainers[i].firstLinkNode = linkNodes[0];
+      this.menuContainers[i].lastLinkNode = linkNodes[linkNodes.length - 1];
     }
-
+    i++;
   }
-
 };
 
 DisclosureMenu.prototype.getMenuContainer = function(node) {
@@ -67,7 +89,6 @@ DisclosureMenu.prototype.getMenuContainer = function(node) {
       return c;
     }
   }
-
   return false;
 };
 
@@ -101,12 +122,180 @@ DisclosureMenu.prototype.toggleExpand = function(menuContainer) {
   }
 };
 
+DisclosureMenu.prototype.expand = function(menuContainer) {
+    this.closeMenus(menuContainer.menuNode);
+    menuContainer.buttonNode.setAttribute('aria-expanded', 'true');
+    this.openMenu(menuContainer.menuNode);
+};
+
+DisclosureMenu.prototype.getLinkIndex = function(menuContainer, link) {
+
+    for (var i = 0; i < menuContainer.linkNodes.length; i++) {
+        if (link === menuContainer.linkNodes[i]) {
+            return i;
+        }
+    }
+
+    return 0;
+};
+
+DisclosureMenu.prototype.setFocusToNextMenu = function(menuContainer) {
+    if (menuContainer != this.lastMenuContainer) {
+        var index = this.menuContainers.indexOf(menuContainer);
+        this.closeMenus();
+        this.menuContainers[index+1].buttonNode.focus();
+    }
+};
+
+DisclosureMenu.prototype.setFocusToPreviousMenu = function(menuContainer) {
+    console.log('[setFocusToPreviousMenu]: ' + menuContainer != this.firstMenuContainer + ' ' + menuContainer.buttonNode.textContent + ' ' + this.firstMenuContainer.buttonNode.textContent);
+    if (menuContainer != this.firstMenuContainer) {
+        var index = this.menuContainers.indexOf(menuContainer);
+        this.closeMenus();
+        this.menuContainers[index-1].buttonNode.focus();
+    }
+};
+
+
+DisclosureMenu.prototype.setFocusToFirstLink = function(menuContainer, currentLink) {
+    if (menuContainer.hasSubMenu) {
+        menuContainer.firstLinkNode.focus();
+    }
+};
+
+DisclosureMenu.prototype.setFocusToLastLink = function(menuContainer, currentLink) {
+    if (menuContainer.hasSubMenu) {
+        menuContainer.lastLinkNode.focus();
+    }
+};
+
+DisclosureMenu.prototype.setFocusToNextLink = function(menuContainer, currentLink) {
+    if (currentLink === menuContainer.lastLinkNode) {
+        menuContainer.firstLinkNode.focus();
+    }
+    else {
+        var index = this.getLinkIndex(menuContainer, currentLink);
+        menuContainer.linkNodes[index+1].focus();        
+    }
+};
+
+DisclosureMenu.prototype.setFocusToPreviousLink = function(menuContainer, currentLink) {
+    if (currentLink === menuContainer.firstLinkNode) {
+        menuContainer.lastLinkNode.focus();
+    }
+    else {
+        var index = this.getLinkIndex(menuContainer, currentLink);
+        menuContainer.linkNodes[index-1].focus();        
+    }
+};
+
+
 /* Event Handlers */
 DisclosureMenu.prototype.handleButtonClick = function(event) {
   var mc = this.getMenuContainer(event.target);
   this.toggleExpand(mc);
-  mc.menuButton.focus();
+  mc.buttonNode.focus();
 };
+
+DisclosureMenu.prototype.handleButtonKeydown = function(event) {
+  var mc = this.getMenuContainer(event.target);
+  var tgt = event.currentTarget,
+    key = event.key,
+    flag = false;
+
+  switch (key) {
+    case 'Esc':
+    case 'Escape':
+        this.closeMenus();
+        flag = true;
+      break;
+
+    case 'Up':
+    case 'ArrowUp':
+        this.expand(mc);
+        this.setFocusToLastLink(mc, tgt);
+        flag = true;
+        break;
+
+    case 'Down':
+    case 'ArrowDown':
+        this.expand(mc);
+        this.setFocusToFirstLink(mc, tgt);
+        flag = true;
+        break;
+
+    case 'Left':
+    case 'ArrowLeft':
+        this.setFocusToPreviousMenu(mc);
+      flag = true;
+      break;
+
+    case 'Right':
+    case 'ArrowRight':
+        this.setFocusToNextMenu(mc);
+      flag = true;
+      break;
+
+
+    default:
+      break;
+  }
+
+  if (flag) {
+    event.stopPropagation();
+    event.preventDefault();
+  }
+};
+
+DisclosureMenu.prototype.handleLinkKeydown = function(event) {
+  var mc = this.getMenuContainer(event.target);
+  var tgt = event.currentTarget,
+    key = event.key,
+    flag = false;
+
+  switch (key) {
+    case 'Esc':
+    case 'Escape':
+        this.closeMenus();
+        mc.buttonNode.focus();
+        flag = true;
+      break;
+
+    case 'Up':
+    case 'ArrowUp':
+      this.setFocusToPreviousLink(mc, tgt);
+      flag = true;
+      break;
+
+    case 'Down':
+    case 'ArrowDown':
+      this.setFocusToNextLink(mc, tgt);
+      flag = true;
+      break;
+
+    case 'Left':
+    case 'ArrowLeft':
+        this.setFocusToPreviousMenu(mc);
+      flag = true;
+      break;
+
+    case 'Right':
+    case 'ArrowRight':
+        this.setFocusToNextMenu(mc);
+      flag = true;
+      break;
+
+
+    default:
+      break;
+  }
+
+  if (flag) {
+    event.stopPropagation();
+    event.preventDefault();
+  }
+};
+
 
 DisclosureMenu.prototype.handleFocusIn = function(event) {
   var mc = this.getMenuContainer(event.target);
--- searchform.php
+++ searchform.php
@@ -3,6 +3,17 @@
     	<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     		<input type="search" size="20" maxlength="128" class="search-field" placeholder="<?php esc_attr_e( 'Search...', 'unlimited' ); ?>" value=""
     		       name="s" title="<?php esc_attr_e( 'Search AITG Pages', 'unlimited' ); ?>"/>
-        	<button type="submit" aria-label="Submit Query"></button>
+              <button type="submit" alt="Submit Query">
+                <svg width="20px" height="20px" viewBox="0 0 256 256" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                  <title>Search Icon: Magnifying Glass</title>
+                  <desc>Created with Sketch</desc>
+                  <defs></defs>
+                  <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                    <g id="Letter" transform="translate(-110.000000, -318.000000)" stroke="currentColor" stroke-width="36">
+                      <path d="M200,545 C152.503512,545 114,506.496488 114,459 C114,411.503512 152.503512,373 200,373 C247.496488,373 286,411.503512 286,459 C286,506.496488 247.496488,545 200,545 Z M286,460 C286,459.447715 286.45476,459 287.00953,459 L386.99047,459 C387.548018,459 388,459.443865 388,460 C388,460.552285 387.54524,461 386.99047,461 L287.00953,461 C286.451982,461 286,460.556135 286,460 Z" id="Combined-Shape" transform="translate(251.000000, 459.000000) rotate(45.000000) translate(-251.000000, -459.000000) "></path>
+                    </g>
+                  </g>
+              </svg>
+            </button>
     	</form>
     </div>
