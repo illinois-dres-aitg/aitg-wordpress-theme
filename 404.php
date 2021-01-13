c9abe8fbd71c4f3b1ad6909ad06ba8df447e54ab
Jon Gunderson <jongund@illinois.edu>
2021-01-13 15:08:52 -0600

    updated 404 page

--- 404.php
+++ 404.php
@@ -1,26 +1,19 @@
 <?php get_header(); ?>
-	<main id="content" class="content">
-	<?php do_action( 'basic_main_content_404_inner_begin' ); ?>
 
-
-    <?php do_action( 'basic_before_page_404_article' ); ?>
-    <div class="post clearfix">
-	    <h1><?php _e( 'Oops! That page can&rsquo;t be found.', 'basic' ); ?></h1>
-
-	    <?php do_action( 'basic_before_page_404_content_box' );  ?>
-        <div class="entry-box clearfix">
-            <p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'basic' ); ?></p>
-            <?php get_search_form(); ?>
-        </div>
-	    <?php do_action( 'basic_after_page_404_content_box' );  ?>
-
+  <main class="flex-row">
+    <div class="left-column">
     </div>
-    <?php do_action( 'basic_after_page_404_article' ); ?>
 
-
-	<?php do_action( 'basic_main_content_404_inner_end' ); ?>
-	</main> 
+    <div class="middle-column search-results">
+      <div class="search-form">
+            <h1><?php _e( 'Oops! That page can&rsquo;t be found.', 'basic' ); ?></h1>
+            <?php get_search_form(); ?>
+      </div>
+    </div>
+    <div class="right-column">
+    </div>
+  </main>
 	<!-- END #content -->
-	
+
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
