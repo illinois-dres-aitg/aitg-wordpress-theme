992c9d3fc05b08e27190c36526b488ae90640df1
Jon Gunderson <jongund@illinois.edu>
2021-02-11 11:43:53 -0600

    updated recent posts

--- sidebar.php
+++ sidebar.php
@@ -53,9 +53,9 @@ $class = ( $mob_sidebar ) ? 'block' : '';
           ) );
 
           foreach ( $categories as $category ) {
-            printf( '<li><a href="%1$s">%2$s</a></li>\n',
+            printf( '<li><a href="%1$s">%2$s</a></li>%3',
               esc_url( get_category_link( $category->term_id ) ),
-              esc_html( $category->name )
+              esc_html( $category->name ), '\n'
             );
           }
         ?>
