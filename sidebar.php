8d15228e02febd511ebae47a24325ad2807dc2b4
Jon Gunderson <jongund@illinois.edu>
2021-02-11 14:36:38 -0600

    updated recent posts

--- sidebar.php
+++ sidebar.php
@@ -53,8 +53,9 @@ $class = ( $mob_sidebar ) ? 'block' : '';
           ) );
 
           foreach ( $categories as $category ) {
-            printf( '<li><a href="%1$s">%2$s</a> <span class="count" title="Number of posts">(%3$s)</span></li>',
+            printf( '<li><a href="%1$s" title="%2$s posts">%3$s</a> <span class="count" >(%4$s)</span></li>',
               esc_url( get_category_link( $category->term_id ) ),
+              esc_html( $category->count ),
               esc_html( $category->name ),
               esc_html( $category->count )
             );
