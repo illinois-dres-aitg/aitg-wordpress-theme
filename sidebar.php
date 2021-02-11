7e2da636679863c1c7ab1c46a048e18050948349
Jon Gunderson <jongund@illinois.edu>
2021-02-11 11:39:54 -0600

    updated recent posts

--- sidebar.php
+++ sidebar.php
@@ -52,12 +52,11 @@ $class = ( $mob_sidebar ) ? 'block' : '';
               'order'   => 'ASC'
           ) );
 
-          if ( $categories ) {
-            foreach( $categories as $category ) :
-        ?>
-            <li><?php esc_html( $category->name ) ?></li>
-        <?php
-            endforeach;
+          foreach ( $categories as $category ) {
+            printf( '<li><a href="%1$s">%2$s</a></li>',
+              esc_url( get_category_link( $category->term_id ) ),
+              esc_html( $category->name )
+            );
           }
         ?>
         </ul>
