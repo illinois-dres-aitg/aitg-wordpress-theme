ff487b426e9f2dd3b9a1ba41afc651ebc03dcd0c
Jon Gunderson <jongund@illinois.edu>
2021-02-15 16:30:15 -0600

    updated content-summary template

--- content-summary.php
+++ content-summary.php
@@ -1,7 +1,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <h2 class="entry-title">
       <a href="<?php the_permalink(); ?>">
-        <?php the_title(); ?> Summary
+        <?php the_title(); ?>
       </a>
     </h2>
 
