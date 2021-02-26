829a24ab266c135a7b82a38f9c034ddb80a874db
Jon Gunderson <jongund@illinois.edu>
2021-02-26 11:28:56 -0600

    updated previous and next links and add tag.php template

--- single.php
+++ single.php
@@ -1,8 +1,5 @@
 <?php get_header();
 
-$no_prev_in_category_link = '<span class="no-link">Previous in Category</span>';
-$no_next_in_category_link = '<span class="no-link">Next in Category</span>';
-
 $prev_in_category_link = get_previous_post_link( '%link', 'Previous in category', true);
 $next_in_category_link = get_next_post_link( '%link', 'Next in category', true);
 
@@ -23,15 +20,11 @@ $next_in_category_link = get_next_post_link( '%link', 'Next in category', true);
         <div class="prev-post">
           <?php if ($prev_in_category_link) {
             echo  $prev_in_category_link;
-          } else {
-            echo  $no_prev_in_category_link;
           } ?>
         </div>
         <div class="next-post">
           <?php if ($next_in_category_link) {
             echo  $next_in_category_link;
-          } else {
-            echo  $no_next_in_category_link;
           } ?>
         </div>
       </nav>
---
+++ tag.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+
+  <div class="flex-row">
+    <div class="left-column">
+    </div>
+    <main class="content">
+    	<?php do_action( 'basic_main_content_inner_begin' ); ?>
+
+            <div class="blog-home-header">
+                <h1>Blog Tags</h1>
+            </div>
+
+        <?php if (have_posts()) : ?>
+
+            <ul>
+                <?php
+                $tags = get_tags();
+                if ( $tags ) :
+                    foreach ( $tags as $tag ) : ?>
+                        <li><a href="<?php echo esc_url( get_tag_link( $tag->term_id ) ); ?>" title="<?php echo esc_attr( $tag->name ); ?>"><?php echo esc_html( $tag->name ); ?></a></li>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+            </ul>
+
+        else: ?>
+
+        	<div class="post clearfix">
+        	    <h2><?php _e( 'Posts not found', 'basic' ); ?></h2>
+        	    <?php get_search_form(); ?>
+        	</div>
+
+        <?php endif; ?>
+
+    	<?php do_action( 'basic_main_content_inner_end' ); ?>
+    </main>
+    <div class="right-column">
+      <?php get_sidebar(); ?>
+    </div>
+  </div>
+
+	<!-- END #content -->
+
+<?php get_footer(); ?>
