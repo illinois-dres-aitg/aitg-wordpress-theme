ef09427474693d18f4200b792303967b4bb00dac
Jon Gunderson <jongund@illinois.edu>
2021-03-11 12:09:18 -0600

    updated CSS

--- single.php
+++ single.php
@@ -19,7 +19,7 @@ $next_in_category_link = get_next_post_link( '%link', 'Next in category >>', tru
       <nav class="current-post-category" aria-label="Current Post Category">
         <div class="prev-post">
           <?php if ($prev_in_category_link) {
-            echo  '$prev_in_category_link;
+            echo  $prev_in_category_link;
           } ?>
         </div>
         <div class="next-post">
