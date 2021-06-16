161fdceff62f1f1e316ffd0c84bb5c1521e8894c
Jon Gunderson <jongund@illinois.edu>
2021-06-16 15:31:28 -0500

    added breadcrumbs

--- index.php
+++ index.php
@@ -3,54 +3,62 @@
   <div class="flex-row">
     <div class="left-column">
     </div>
-    <main class="content">
-    	<?php do_action( 'basic_main_content_inner_begin' ); ?>
+    <div class="middle-column">
+      <div class="breadcrumb">
+        <!-- @ breadcrumb trail of links indicating location of
+             current page in menu structure -->
+        <?php echo do_shortcode( '[breadcrumb]' ); ?>
+      </div>
 
-        <?php if ( is_home() && 'customtitle' == get_theme_mod( 'home_h1_type', 'sitetitle' )  ) { ?>
-            <div class="blog-home-header">
-                <h1><?php echo esc_html( get_theme_mod( 'custom_home_h1', get_bloginfo('sitetitle') ) ); ?></h1>
-            </div>
-        <?php } ?>
+      <main class="content">
+        	<?php do_action( 'basic_main_content_inner_begin' ); ?>
 
-        <?php if (have_posts()) : ?>
-
-
-            <?php
-                $category = single_cat_title( '', false );
-                if ($category) {?>
-                <h1><?php echo $category ?> Blog Posts</h1>
-            <?php } else { ?>
-                <h1>All Blog Posts</h1>
+            <?php if ( is_home() && 'customtitle' == get_theme_mod( 'home_h1_type', 'sitetitle' )  ) { ?>
+                <div class="blog-home-header">
+                    <h1><?php echo esc_html( get_theme_mod( 'custom_home_h1', get_bloginfo('sitetitle') ) ); ?></h1>
+                </div>
             <?php } ?>
 
-            <?php
-
-        	while (have_posts()) : the_post();
-
-        		get_template_part( 'content-summary' );
-
-        	endwhile; ?>
-
-        	<?php
-
-        	the_posts_pagination( apply_filters( 'basic_posts_pagination_args', array(
-        		'mid_size' => 2,
-        		'prev_text' => __( '&laquo; Prev', 'basic'),
-        		'next_text' => __( 'Next &raquo;', 'basic'),
-        	) ) );
+            <?php if (have_posts()) : ?>
 
 
-        else: ?>
+                <?php
+                    $category = single_cat_title( '', false );
+                    if ($category) {?>
+                    <h1><?php echo $category ?> Blog Posts</h1>
+                <?php } else { ?>
+                    <h1>All Blog Posts</h1>
+                <?php } ?>
 
-        	<div class="post clearfix">
-        	    <h2><?php _e( 'Posts not found', 'basic' ); ?></h2>
-        	    <?php get_search_form(); ?>
-        	</div>
+                <?php
 
-        <?php endif; ?>
+            	while (have_posts()) : the_post();
 
-    	<?php do_action( 'basic_main_content_inner_end' ); ?>
-    </main>
+            		get_template_part( 'content-summary' );
+
+            	endwhile; ?>
+
+            	<?php
+
+            	the_posts_pagination( apply_filters( 'basic_posts_pagination_args', array(
+            		'mid_size' => 2,
+            		'prev_text' => __( '&laquo; Prev', 'basic'),
+            		'next_text' => __( 'Next &raquo;', 'basic'),
+            	) ) );
+
+
+            else: ?>
+
+            	<div class="post clearfix">
+            	    <h2><?php _e( 'Posts not found', 'basic' ); ?></h2>
+            	    <?php get_search_form(); ?>
+            	</div>
+
+            <?php endif; ?>
+
+        	<?php do_action( 'basic_main_content_inner_end' ); ?>
+        </main>
+    </div>
     <div class="right-column">
       <?php get_sidebar(); ?>
     </div>
--- page.php
+++ page.php
@@ -2,38 +2,47 @@
   <div class="flex-row">
     <div class="left-column">
     </div>
-    <main class="content">
-		<?php while ( have_posts() ) : the_post(); ?>
+    <div class="middle-column">
+      <div class="breadcrumb">
+        <!-- @ breadcrumb trail of links indicating location of
+             current page in menu structure -->
+        <?php echo do_shortcode( '[breadcrumb]' ); ?>
+      </div>
 
-			<?php do_action( 'basic_before_page_article' ); ?>
-			<article class="post page" id="pageid-<?php the_ID(); ?>">
+      <main class="content">
 
-				<?php do_action( 'basic_before_page_title' );  ?>
-				<h1><?php the_title(); ?></h1>
-				<?php do_action( 'basic_after_page_title' );  ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php do_action( 'basic_before_page_content_box' );  ?>
-				<div class="entry-box clearfix">
-					<?php do_action( 'basic_before_page_content' );  ?>
-					<?php the_content(); ?>
-					<?php do_action( 'basic_after_page_content' );  ?>
-				</div>
-				<?php do_action( 'basic_after_page_content_box' );  ?>
+				<?php do_action( 'basic_before_page_article' ); ?>
+				<article class="post page" id="pageid-<?php the_ID(); ?>">
 
-			</article>
-			<?php do_action( 'basic_after_page_article' ); ?>
+					<?php do_action( 'basic_before_page_title' );  ?>
+					<h1><?php the_title(); ?></h1>
+					<?php do_action( 'basic_after_page_title' );  ?>
+
+					<?php do_action( 'basic_before_page_content_box' );  ?>
+					<div class="entry-box clearfix">
+						<?php do_action( 'basic_before_page_content' );  ?>
+						<?php the_content(); ?>
+						<?php do_action( 'basic_after_page_content' );  ?>
+					</div>
+					<?php do_action( 'basic_after_page_content_box' );  ?>
+
+				</article>
+				<?php do_action( 'basic_after_page_article' ); ?>
 
 
-			<?php
+				<?php
 
-			if ( comments_open() || get_comments_number() ) {
-				do_action( 'basic_before_page_comments_area' );
-				comments_template();
-				do_action( 'basic_after_page_comments_area' );
-			}
+				if ( comments_open() || get_comments_number() ) {
+					do_action( 'basic_before_page_comments_area' );
+					comments_template();
+					do_action( 'basic_after_page_comments_area' );
+				}
 
-		endwhile; ?>
-    </main>
+			endwhile; ?>
+	    </main>
+	  </div>
     <div class="right-column">
     </div>
   </div>
